785c2816060c78a5fc6a86fa424c742ee63d0c15
thibault corouge <thibaultcorouge@minidethibault.home>
2022-03-28 09:52:56 +0200

    controllers modification

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,25 +1,24 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const user = require('../models.user');
+const User = require('../models/user');
 
 exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 1000)
-        .then(hash =>{
-            const user = new user({
+    bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+            const user = new User({
                 email: req.body.email,
                 password: hash
             });
-
             user.save()
             .then(() => res.status(201).json({ message: 'utilisateur créé'}))
             .catch(error => res.status(400).json({ error}));
         })
-        .catch(error => res.status(500).json({ error }));
+        .catch(error => res.status(500).json({ error })); // ici erreur
 };
 
 
 exports.login = (req, res, next) => {
-    user.findOne({ email: req.body.email})
+    User.findOne({ email: req.body.email})
         .then(user => {
             if(!user) {
                 return res.status(401).json({ error : 'Utilisateur non trouvé'})
@@ -31,7 +30,11 @@ exports.login = (req, res, next) => {
                 }
                 res.status(200).json({
                     userId: user._id,
-                    token: jwt.sign({ userId: user._id}, 'RANDOM_TOKEN_SECRET', { expiresIn: '24h'})
+                    token: jwt.sign(
+                        { userId: user._id},
+                        'RANDOM_TOKEN_SECRET', 
+                        { expiresIn: '24h'}
+                    )
                 });
             })
             .catch(error => res.status(500).json({ error }));
